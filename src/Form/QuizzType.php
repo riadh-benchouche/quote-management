fec5ab0a9ced33ac86e33f12b214c0074b14ce9f
Adrien Morin <morin.adrien@gmail.com>
2023-02-23 15:00:54 +0100

    ADD Form Type

--- src/Form/QuizzType.php
+++ src/Form/QuizzType.php
@@ -2,8 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
+use App\Entity\Question;
 use App\Entity\Quizz;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +18,22 @@ class QuizzType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('categories')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du quizz',
+                'attr' => [
+                    'placeholder' => 'Nom du quizz'
+                ]
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'multiple' => true,
+                'choice_label' => 'name'
+            ])
+            ->add('questions', EntityType::class, [
+                'class' => Question::class,
+                'multiple' => true,
+                'choice_label' => 'question'
+            ])
         ;
     }
 
