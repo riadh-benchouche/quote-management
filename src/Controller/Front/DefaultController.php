d40bfa6135fb07be28044dfcbb947527b8901ed9
riadh-fstck <riadh@fstck.co>
2023-07-11 00:48:59 +0200

    add: stats to dashboard for employee and accountant

--- src/Controller/Front/DefaultController.php
+++ src/Controller/Front/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\DevisRepository;
+use App\Repository\FactureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +12,34 @@ use App\Entity\User;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'front_default_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(FactureRepository $factureRepository, DevisRepository $devisRepository): Response
     {
         /** @var User $user */
         $user = $this->getUser();
 
+        // get last 5 factures
+        $factures = $factureRepository->findBy(
+            [],
+            ['date' => 'DESC'],
+            5,
+            0
+        );
+        $devis = $devisRepository->findBy(
+            [],
+            ['date' => 'DESC'],
+            5,
+            0
+        );
+        $totalFactures = $factureRepository->findAll() ? count($factureRepository->findAll()) : 0;
+        $totalDevis = $devisRepository->findAll() ? count($devisRepository->findAll()) : 0;
+
         if ($user && $user->isVerified()) {
-            return $this->render('front/default/index.html.twig');
+            return $this->render('front/default/index.html.twig', [
+                'totalFactures' => $totalFactures,
+                'totalDevis' => $totalDevis,
+                'factures' => $factures,
+                'devis' => $devis,
+            ]);
         } else {
             return $this->render('front/default/please-verify-mail.html.twig');
         }
